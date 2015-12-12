9a3b4503e5aabe7ab294ef9a4a533af891fe04d0
jpoeng <jenniferpoeng@gmail.com>
2015-12-11 22:34:25 -0800

    add rspec test for drinks model

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,7 +1,10 @@
 class Drink < ActiveRecord::Base
+
+  ERRCODE = 1
+
   validates_presence_of :name, :cups
   validate :number_of_cups_should_be_greater_than_zero, :number_of_cups_should_be_less_than_ten
-  validates_numericality_of :cups, :greater_than => 0, :less_than => 10
+  validates_numericality_of :cups
 
   def number_of_cups_should_be_less_than_ten
     if cups >= 10
--- spec/models/drink_spec.rb
+++ spec/models/drink_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Drink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# describe Drink, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe "number of cups" do
+  it "should be between 1 and 9" do
+    drink = Drink.new(name: "latte", brand: "Starbucks", cups: -1)
+    drink.valid?
+    expect(drink.errors[:cups].size).to eq 1
+  end
+
+  it "should enforce drink to have a name" do
+    drink = Drink.new(name: "", brand: "Starbucks", cups: 1)
+    drink.valid?
+    expect(drink.errors[:name].size).to eq 1
+  end
 end
