4f2b9af5997377e4715ba772f53ef4c2faf823f9
jpoeng <jenniferpoeng@gmail.com>
2015-12-14 00:12:41 -0800

    Add geoplaces to display location of users on map

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -9,6 +9,12 @@ class DrinksController < ApplicationController
   # GET /drinks
   # GET /drinks.json
   def index
+    @users = User.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
+
     if params[:brand]
       @drinks = Drink.where(brand: params[:brand])
     else
